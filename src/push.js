3ef736f9cec6388f8353987f3a6c88a50b3132cf
nokazn <baleariclemon@gmail.com>
2019-09-03 12:12:55 +0000

    add webpack, babel, async, canvas

---
+++ src/push.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const line = require('@line/bot-sdk');
+require('dotenv').config();
+
+function pushMessage (msg) {
+  const client = new line.Client({
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+  });
+
+  const message = {
+    type: 'text',
+    text: msg
+  };
+
+  client.pushMessage(process.env.USER_ID, message).then((res) => {
+    console.info(res);
+  }).catch((err) => {
+    console.error(err);
+  });
+};
+
+module.exports.pushMessage = pushMessage;
