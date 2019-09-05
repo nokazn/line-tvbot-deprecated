282e22af1a5ab1ae1aa8fd5d5c201209a60cbdce
nokazn <baleariclemon@gmail.com>
2019-09-05 14:38:42 +0000

    configure webpack

--- src/push.js
+++ src/push.js
@@ -1,9 +1,14 @@
 'use strict'
 
 const line = require('@line/bot-sdk');
-require('dotenv').config();
+const dotenvConfig = require('dotenv').config();
 
 function pushMessage (msg) {
+  if (typeof process.env.CHANNEL_ACCESS_TOKEN === 'undefined' || typeof process.env.CHANNEL_SECRET === 'undefined') {
+    console.error(dotenvConfig);
+    console.error({ CHANNEL_ACCESS_TOKEN: process.env.CHANNEL_ACCESS_TOKEN });
+    console.error({ CHANNEL_SECRET: process.env.CHANNEL_SECRET });
+  }
   const client = new line.Client({
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
     channelSecret: process.env.CHANNEL_SECRET
