63d86f453123a5e79d305b168cb536e020d88d11
nokazn <baleariclemon@gmail.com>
2019-09-25 13:02:25 +0000

    init ts

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,22 @@ const path = require('path');
 module.exports = {
   target: 'node',
   mode: 'development',
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname),
     filename: './dist/index.js',
     libraryTarget: 'umd'
   },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
   resolve: {
-    extensions: ['.js']
+    extensions: ['.ts']
   },
   externals: {
     canvas: 'commonjs canvas'
